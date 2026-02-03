dfd81630d4e3af5dfa6e21fecb145c7268f677e7
UppiliSrinivas <uppilisrinivasanecs@gmail.com>
2026-02-03 22:30:19 +0530

    debounce test case completed

--- test/features/debounce.test.ts
+++ test/features/debounce.test.ts
@@ -2,13 +2,16 @@ import { describe, it, expect, vi, afterEach, beforeEach } from "vitest";
 import debounce from "../../src/features/debounce";
 
 const fn = vi.fn((_signal: AbortSignal, v: string) => v);
+
 beforeEach(() => {
   vi.useFakeTimers();
 });
+
 afterEach(() => {
   vi.useRealTimers();
   vi.restoreAllMocks();
 });
+
 describe("debounce", () => {
   it("Debounce - trailing, executes only once after delay", async () => {
     const d = debounce(fn, 500);
@@ -35,7 +38,54 @@ describe("debounce", () => {
     d("C");
 
     // should be called immediately
-    expect(fn).toHaveBeenCalledTimes(3);
+    expect(fn).toHaveBeenCalledTimes(1);
     expect(fn).toHaveBeenCalledWith(expect.any(AbortSignal), "A");
+  });
+
+  it("Debounce - leading and trailing, executes both", async () => {
+    const d = debounce(fn, 500, { leading: true, trailing: true });
+
+    d("A");
+    d("B");
+    d("C");
+
+    // should be called immediately
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(fn).toHaveBeenCalledWith(expect.any(AbortSignal), "A");
+
+    // move time forward
+    vi.advanceTimersByTime(500);
+
+    expect(fn).toHaveBeenCalledTimes(2);
+    expect(fn).toHaveBeenLastCalledWith(expect.any(AbortSignal), "C");
+  });
+
+  it("Debounce - cancel, should not execute", async () => {
+    const d = debounce(fn, 500);
+
+    d("A");
+    d("B");
+
+    d.cancel();
+
+    // move time forward
+    vi.advanceTimersByTime(500);
+
+    expect(fn).not.toHaveBeenCalled();
+  });
+
+  it("Debounce - flush, should execute immediately", async () => {
+    const d = debounce(fn, 500);
+
+    d("A");
+    d("B");
+
+    // nothing yet
+    expect(fn).not.toHaveBeenCalled();
+
+    d.flush();
+
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(fn).toHaveBeenCalledWith(expect.any(AbortSignal), "B");
   });
 });
