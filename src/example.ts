4b22efedbc51c0b803c93ad572c5104600966d55
UppiliSrinivas <uppilisrinivasanecs@gmail.com>
2026-02-04 20:17:02 +0530

    update example file

--- src/example.ts
+++ src/example.ts
@@ -1,5 +1,5 @@
 import debounce from "./features/debounce";
-import throttle from "./features/thottle";
+import throttle from "./features/throttle";
 
 const log = (_signal: AbortSignal, msg: string) => {
   console.log(`${msg}`);
@@ -13,7 +13,7 @@ const d1 = debounce(log, 1000, {
 
 d1("A");
 d1("B");
-d1("C");
+d1("Debounce: leading: false, trailing: true => C");
 
 // Debounce: leading: true, trailing: false
 const d2 = debounce(log, 1000, {
@@ -21,19 +21,19 @@ const d2 = debounce(log, 1000, {
   trailing: false,
 });
 
-d2("Leading TRUE : A");
-d2("Leading TRUE : B");
-d2("Leading TRUE : C");
-/* 
+d2("Debounce: leading: true, trailing : A");
+d2("B");
+d2("C");
+
 // Debounce: leading: true, trailing: true
 const d3 = debounce(log, 1000, {
   leading: true,
   trailing: true,
 });
 
-d3("A");
+d3("Debounce: leading: true, trailing: true => A");
 d3("B");
-d3("C");
+d3("Debounce: leading: true, trailing: true => C");
 
 // Debounce: cancel()
 const d4 = debounce(log, 1000);
@@ -46,7 +46,7 @@ d4.cancel();
 // Debounce: flush()
 const d5 = debounce(log, 1000);
 
-d5("A");
+d5("Debounce: flush() => A");
 
 setTimeout(() => {
   d5.flush();
@@ -58,9 +58,9 @@ const t1 = throttle(log, 1000, {
   trailing: false,
 });
 
-t1("A");
-t1("B");
-t1("C");
+t1("Throttle: leading: true, trailing: false => A");
+t1("Throttle: leading: true, trailing: false => B");
+t1("Throttle: leading: true, trailing: false => C");
 
 // Throttle: leading: false, trailing: true
 const t2 = throttle(log, 1000, {
@@ -68,9 +68,9 @@ const t2 = throttle(log, 1000, {
   trailing: true,
 });
 
-t2("A");
-t2("B");
-t2("C");
+t2("Throttle: leading: false, trailing: true => A");
+t2("Throttle: leading: false, trailing: true => B");
+t2("Throttle: leading: false, trailing: true => C");
 
 // Throttle: leading: true, trailing: true
 const t3 = throttle(log, 1000, {
@@ -78,24 +78,24 @@ const t3 = throttle(log, 1000, {
   trailing: true,
 });
 
-t3("A");
-t3("B");
-t3("C");
+t3("Throttle: leading: true, trailing: true => A");
+t3("Throttle: leading: true, trailing: true => B");
+t3("Throttle: leading: true, trailing: true => C");
 
 // Throttle: cancel()
 const t4 = throttle(log, 1000);
 
-t4("A");
-t4("B");
+t4("Throttle: cancel() => A");
+t4("Throttle: cancel() => B");
 
 t4.cancel();
 
 // Throttle: flush()
-const t5 = throttle(log, 1000);
+const t5 = throttle(log, 1000, { leading: false, trailing: false });
 
-t5("A");
-t5("B");
+t5("Throttle: flush() => A");
+t5("Throttle: flush() => B");
 
 setTimeout(() => {
   t5.flush();
-}, 300); */
+}, 300);
